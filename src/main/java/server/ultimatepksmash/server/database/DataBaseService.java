6cb72b9b3e51b257c571ea41d3d8680b967d28ce
Kuba Drałus <kubadralus@gmail.com>
2024-01-16 18:55:20 +0100

    better connection handling

--- src/main/java/server/ultimatepksmash/server/database/DataBaseService.java
+++ src/main/java/server/ultimatepksmash/server/database/DataBaseService.java
@@ -1,6 +1,8 @@
 package server.ultimatepksmash.server.database;
 
 import lombok.AllArgsConstructor;
+import server.ultimatepksmash.server.database.user.User;
+import server.ultimatepksmash.server.database.user.UserService;
 
 import java.sql.*;
 
@@ -13,8 +15,9 @@ public class DataBaseService {
     public static void connectToDatabase() throws SQLException {
         String dbName = "ultimatepksmashers";
         String url = "jdbc:postgresql://localhost:5432/postgres";
-        connection = DriverManager.getConnection(url, "postgres", "postgres");
+       
         try {
+            connection = DriverManager.getConnection(url, "postgres", "postgres");
             PreparedStatement statement = connection.prepareStatement("CREATE DATABASE " + dbName);
             statement.executeUpdate();
             System.out.println("created database " + dbName + " schema: " + connection.getSchema());
@@ -30,5 +33,13 @@ public class DataBaseService {
     public static void closeConnection() throws SQLException {
         connection.close();
         System.out.println("connection closed");
+    }
+    
+    public static void main(String[] args) throws SQLException {
+        connectToDatabase();
+        UserService userService = new UserService();
+        userService.addUser(new User("nigger","afboboa","123"));
+        
+        closeConnection();
     }
 }
