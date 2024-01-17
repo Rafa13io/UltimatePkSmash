bae517177f2c8655672dcdd19a68f52e2ef45a87
Kuba Drałus <kubadralus@gmail.com>
2024-01-17 23:59:39 +0100

    results for a user

--- src/main/java/server/ultimatepksmash/server/database/results/ResultService.java
+++ src/main/java/server/ultimatepksmash/server/database/results/ResultService.java
@@ -38,6 +38,24 @@ public class ResultService {
         return results1vs1;
     }
     
+    public List<Result1vs1> getUserResults1vs1(Long userId) throws SQLException {
+        String sql = "SELECT * FROM p_result_1vs1 WHERE ? IN (winner_id, loser_id);";
+        PreparedStatement getResults1vs1 = connection.prepareStatement(sql);
+        getResults1vs1.setLong(1, userId);
+        ResultSet resultSet = getResults1vs1.executeQuery();
+        
+        List<Result1vs1> results1vs1 = new ArrayList<>();
+        // Iterate over the rows
+        while (resultSet.next()) {
+            Result1vs1 result1vs1 = new Result1vs1();
+            mapResult1vs1(result1vs1, resultSet);
+            results1vs1.add(result1vs1);
+        }
+        resultSet.close();
+        getResults1vs1.close();
+        return results1vs1;
+    }
+    
     public void addResult1vs1(Result1vs1 result1vs1) throws SQLException {
         if (Objects.equals(result1vs1.getWinnerId(), result1vs1.getLoserId())) {
             throw new RuntimeException("A player cannot verse himself lol");
@@ -67,6 +85,9 @@ public class ResultService {
         result1vs1.setLoserId(resultSet.getLong("loser_id"));
     }
     
+    
+    // -------------------- 2vs2 ---------------------
+    
     public List<Result2vs2> getResults2vs2() throws SQLException {
         String sql = "SELECT * FROM p_result_2vs2";
         PreparedStatement getResults2vs2 = connection.prepareStatement(sql);
@@ -81,6 +102,24 @@ public class ResultService {
         }
         resultSet.close();
         getResults2vs2.close();
+        return results2vs2;
+    }
+    
+    public List<Result2vs2> getUserResults2vs2(Long userId) throws SQLException {
+        String sql = "SELECT * FROM p_result_2vs2 WHERE ? IN (winner1_id, loser1_id, winner2_id, loser2_id);";
+        PreparedStatement getResults1vs1 = connection.prepareStatement(sql);
+        getResults1vs1.setLong(1, userId);
+        ResultSet resultSet = getResults1vs1.executeQuery();
+        
+        List<Result2vs2> results2vs2 = new ArrayList<>();
+        // Iterate over the rows
+        while (resultSet.next()) {
+            Result2vs2 result2vs2 = new Result2vs2();
+            mapResult2vs2(result2vs2, resultSet);
+            results2vs2.add(result2vs2);
+        }
+        resultSet.close();
+        getResults1vs1.close();
         return results2vs2;
     }
     
